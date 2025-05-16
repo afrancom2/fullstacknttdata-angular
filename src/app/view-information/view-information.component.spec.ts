95e7c084a86a9a9807274add135b54efd44ce04d
Andres Franco <afrancom2@gmail.com>
2025-05-16 16:22:27 -0500

    add

--- src/app/view-information/view-information.component.spec.ts
+++ src/app/view-information/view-information.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ViewInformationComponent } from './view-information.component';
+import {Root, Body} from "../model/model";
 
 describe('ViewInformationComponent', () => {
   let component: ViewInformationComponent;
@@ -19,5 +20,27 @@ describe('ViewInformationComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should view oninit', () => {
+    const data: Body = {
+      firstName: "andres",
+      secondName: "felipe",
+      firstLastName: "franco",
+      secondLastName: "monroy",
+      phone: 123123,
+      address: "calle falsa 123",
+      city: "medellin"
+    }
+    const root: Root = {
+      code: 0,
+      httpStatus: "1",
+      message: "successfull",
+      body: data
+    }
+
+    component.data = data;
+
+    expect(component.data).toBe(data);
   });
 });
